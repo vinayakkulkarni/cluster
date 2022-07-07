3297f2edd06fc6682850ce75d4e0d0e9200c3fe5
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-07-07 18:02:45 +0530

    fix: gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cluster/',
   plugins: [vue()],
   resolve: {
     alias: {
