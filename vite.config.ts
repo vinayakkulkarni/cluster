ec820991a0696c0c5e4133a1624a616540e3f9af
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-11-02 11:11:19 +0530

    chore: minor housekeeping 🎉
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cluster/',
+  base: process.env.NODE_ENV === 'production' ? '/cluster/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
